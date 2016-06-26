be823f5f52c118e15930fcf41f83c235f58335f0
Andrew Christofi <victoryspecificationii@gmail.com>
2016-06-26 20:58:42 +0100

    project page basic implementation and intent
    Created a basic implementation of the projects screen, created intents
    to switch from landing screen to projects screen.

--- app/src/main/java/com/perrinn/client/projectsActivity.java
+++ app/src/main/java/com/perrinn/client/projectsActivity.java
@@ -8,12 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class LandingActivity extends AppCompatActivity {
+public class projectsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing);
+        setContentView(R.layout.activity_projects);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,9 +28,6 @@ public class LandingActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    public void projectsPageIntent(View view) {
-        Intent intent = new Intent(this, projectsActivity.class);
-        startActivity(intent);
-    }
+    Intent intent = getIntent();
 
 }
