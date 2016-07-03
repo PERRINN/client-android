2390d9bd33e861138dbefa9c6ae31938a7af3564
Andrew Christofi <victoryspecificationii@gmail.com>
2016-07-03 10:56:15 +0300

    created stubs for functions

--- app/src/main/java/com/perrinn/client/fragments/CreateNewProjectFragment.java
+++ app/src/main/java/com/perrinn/client/fragments/CreateNewProjectFragment.java
@@ -1,7 +1,80 @@
 package com.perrinn.client.fragments;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.perrinn.client.R;
 /**
- * Created by Antreas Christofi on 7/3/2016.
+ * A fragment containing a simple view.
  */
-public class CreateNewProjectFragment {
+public class CreateNewProjectFragment extends Fragment {
+    private CreateNewProjectFragmentInteractionListener mListener;
+
+    public CreateNewProjectFragment() {
+    }
+    /*
+    * //////////////////////////////////////////////////
+    * // Overrided methods
+    * /////////////////////////////////////////////////
+    */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+        //on create should populate the project owner button with the current user's name/nick
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_landing, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    /*
+    * //////////////////////////////////////////////////
+    * // Public methods
+    * /////////////////////////////////////////////////
+    */
+    /**
+     * This method creates a new instance of the fragment and return it to the caller.
+     *
+     * @return LoadingFragment the new fragment created.
+     * */
+    public static CreateNewProjectFragment newInstance(){
+        CreateNewProjectFragment fragment = new CreateNewProjectFragment();
+
+        return fragment;
+    }
+
+    //function for inserting members
+    public void insertMembersToProject(){
+
+    }
+
+    //function for inserting team
+    public void insertTeamsToProject(){
+
+    }
+
+    //grab owner name and put as
+    public void getOwnerName(){
+
+    }
+
+    public interface CreateNewProjectFragmentInteractionListener{
+        void onTextInteraction();
+    }
+
 }
