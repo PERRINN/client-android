6cebbf15500a1e627146b7dfa61309b89677032b
Antreas Christofi <victoryspecificationii@gmail.com>
2016-07-23 00:23:41 +0300

    added dock to team screen xml

---
+++ app/src/main/java/com/perrinn/client/fragments/TeamMembersFragment.java
@@ -0,0 +1,18 @@
+package com.perrinn.client.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by Antreas Christofi on 7/22/2016.
+ */
+public class TeamMembersFragment extends Fragment {
+
+    /*
+    *Constructor
+     */
+    public TeamMembersFragment(){
+
+    }
+
+
+}
