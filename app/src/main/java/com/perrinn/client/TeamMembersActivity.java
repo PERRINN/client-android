399b149399d446c3cb489def0664d0befa52088f
Antreas Christofi <victoryspecificationii@gmail.com>
2016-07-28 15:37:10 +0300

    re arranged items, deleted some other unnecessary items

--- app/src/main/java/com/perrinn/client/TeamMembersActivity.java
+++ app/src/main/java/com/perrinn/client/TeamMembersActivity.java
@@ -43,7 +43,6 @@ public class TeamMembersActivity extends Activity {
 		Bitmap images02Icon = BitmapFactory.decodeResource(this.getResources(), R.drawable.icon_images_02);
 		Bitmap mapsIcon = BitmapFactory.decodeResource(this.getResources(), R.drawable.icon_maps_01);
 		Bitmap microphoneIcon = BitmapFactory.decodeResource(this.getResources(), R.drawable.icon_mic);
-		Bitmap plusIcon = BitmapFactory.decodeResource(this.getResources(), R.drawable.icon_plus);
 		Bitmap project01Icon = BitmapFactory.decodeResource(this.getResources(), R.drawable.icon_project_01);
 		Bitmap project02Icon = BitmapFactory.decodeResource(this.getResources(), R.drawable.icon_project_02);
 		Bitmap public01Icon = BitmapFactory.decodeResource(this.getResources(), R.drawable.icon_public_01);
@@ -68,10 +67,9 @@ public class TeamMembersActivity extends Activity {
 		gridArray.add(new Item(guestIcon,"Guest"));
 		gridArray.add(new Item(activityIcon,"Activity"));
 		gridArray.add(new Item(images02Icon,"Images2"));
-		gridArray.add(new Item(plusIcon,"Plus"));
-		gridArray.add(new Item(project01Icon,"Project1"));
-		gridArray.add(new Item(project02Icon,"Project2"));
-		gridArray.add(new Item(public01Icon,"Public1"));
+		//gridArray.add(new Item(project01Icon,"Project1"));
+		//gridArray.add(new Item(project02Icon,"Project2"));
+		//gridArray.add(new Item(public01Icon,"Public1"));
 
 		gridArray.add(new Item(documentsIcon,"Documents"));
 		gridArray.add(new Item(calendarIcon,"Calendar"));
