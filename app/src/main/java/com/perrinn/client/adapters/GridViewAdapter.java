da677ba6bf3e10569752677b86ee1c41ee636e06
Alessandro <alessandro.silacci@edu.hefr.ch>
2016-08-05 12:30:41 +0200

    Moved the Team Members Grid Adapter in the adapters package.

--- app/src/main/java/com/perrinn/client/adapters/GridViewAdapter.java
+++ app/src/main/java/com/perrinn/client/adapters/GridViewAdapter.java
@@ -1,17 +1,19 @@
-package com.perrinn.client;
+package com.perrinn.client.adapters;
 
 import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.perrinn.client.R;
+import com.perrinn.client.beans.Item;
 
 /**
  * Created by Antreas Christofi on 26-07-2016.
